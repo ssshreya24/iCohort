e0f3b63d679df9cdd8cde04446ee8a216882c514
ssshreya24 <shreya131268@gmail.com>
2026-04-21 22:30:29 +0530

    Initial Commit

---
+++ iCohort-New/iCohort_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCohort_NewApp.swift
+//  iCohort-New
+//
+//  Created by admin100 on 21/04/26.
+//
+
+import SwiftUI
+
+@main
+struct iCohort_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
